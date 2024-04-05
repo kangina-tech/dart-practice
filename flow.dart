a13a460eb104e2f8b91b3596bd919581e87dcc5d
Kangina <85179015+kangina-tech@users.noreply.github.com>
2024-04-05 12:01:18 +0300

    dart control flow practice

---
+++ flow.dart
@@ -0,0 +1,10 @@
+void main() {
+  int num = 10;
+  if(num > 10) {
+    print("Your number is greater than 10");
+  } else if(num < 10) {
+    print("Your number is less than ten");
+  } else {
+    print("Your number is equal to 10");
+  }
+}
